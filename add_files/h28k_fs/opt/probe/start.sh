3ffe533a3f9b5f0644a18caa554925608ce00fb6
hexm <hexm@trustmo.com>
2024-11-15 11:42:40 +0800

    添加看门狗

--- add_files/h28k_fs/opt/probe/start.sh
+++ add_files/h28k_fs/opt/probe/start.sh
@@ -1,5 +1,6 @@
 lanac=/opt/probe/sensor/lanac
 update=/opt/probe/sensor/update
+watchdog=/opt/probe/sensor/watchdog
 
 # 启动lanac主进程
 echo "Starting $lanac"
@@ -17,4 +18,13 @@ if [ ! -f $update ];then
     echo "Info: No $update"
 else
     $update -d
+fi
+
+
+echo "Starting $watchdog"
+$watchdog -v
+if [ ! -f $watchdog ];then
+    echo "Info: No $watchdog"
+else
+    $watchdog -d
 fi
---
+++ add_files/h28k_fs/opt/probe/watchdog.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+while :;do
+  echo "A" > /dev/watchdog0
+  sleep 30
+done
