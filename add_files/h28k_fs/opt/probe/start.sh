f55895807239ccf2234f48725d58ac88436fd344
hexm <hexm@trustmo.com>
2024-12-25 17:54:10 +0800

    看门狗进程与lanac安装包解耦

--- add_files/h28k_fs/opt/probe/start.sh
+++ add_files/h28k_fs/opt/probe/start.sh
@@ -1,6 +1,5 @@
 lanac=/opt/probe/sensor/lanac
 update=/opt/probe/sensor/update
-watchdog=/opt/probe/sensor/watchdog
 
 # 启动lanac主进程
 echo "Starting $lanac"
@@ -18,13 +17,4 @@ if [ ! -f $update ];then
     echo "Info: No $update"
 else
     $update -d
-fi
-
-
-echo "Starting $watchdog"
-$watchdog -v
-if [ ! -f $watchdog ];then
-    echo "Info: No $watchdog"
-else
-    $watchdog -d
 fi
